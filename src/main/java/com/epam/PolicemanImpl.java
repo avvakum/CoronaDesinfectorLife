c9b6d8f638c4bdff3c92258a197b67736e811e30
AVVA <abba501@mail.ru>
2025-06-26 12:28:48 +0300

    26.06.2025

--- src/main/java/com/epam/PolicemanImpl.java
+++ src/main/java/com/epam/PolicemanImpl.java
@@ -1,6 +1,17 @@
 package com.epam;
 
+import javax.annotation.PostConstruct;
+
 public class PolicemanImpl implements Policeman {
+
+    @InjectByType
+    private Recommendator recommendator;
+
+    @PostConstruct
+    public void init() {
+        System.out.println(recommendator.getClass());
+    }
+
     @Override
     public void makePeopleLeaveRoom() {
         System.out.println("Пиф паф, бах бах, кыш кыш!!!");
