d4b42c4e2f48789d368b66c8f218a1782601bfaf
AVVA <abba501@mail.ru>
2025-12-25 11:10:46 +0300

    25.12.2025

---
+++ src/main/java/com/avva_08/AngryPoliceman.java
@@ -0,0 +1,8 @@
+package com.avva_08;
+
+public class AngryPoliceman implements Policeman {
+    @Override
+    public void makePeopleLeaveRoom() {
+        System.out.println("Всех убью! Вон пошли!");
+    }
+}
