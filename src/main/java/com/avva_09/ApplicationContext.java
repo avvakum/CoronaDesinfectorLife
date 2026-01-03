96c19a441bf3430efc6cdabc664bf0e7fd10f608
AVVA <abba501@mail.ru>
2026-01-03 12:05:32 +0300

    03.01.2026

---
+++ src/main/java/com/avva_09/ApplicationContext.java
@@ -0,0 +1,41 @@
+package com.avva_09;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ApplicationContext {
+    @Setter
+    private ObjectFactory factory;
+    private Map<Class, Object> cashe = new ConcurrentHashMap<>();
+    @Getter
+    private Config config;
+
+    public ApplicationContext(Config config) {
+        this.config = config;
+    }
+
+    public <T> T getObject(Class<T> type) {
+
+        if (cashe.containsKey(type)) {
+            return (T) cashe.get(type);
+        }
+
+        Class<? extends T> implClass = type;
+
+        if (type.isInterface()) {
+            implClass = config.getImplClass(type);
+        }
+
+        T t = factory.ceateObject(implClass);
+
+        if (implClass.isAnnotationPresent(Singleton.class)) {
+            cashe.put(type, t);
+        }
+
+        return t;
+    }
+}
+
