71d68e273b7e0b0632d566db9172103bd0a421a2
AVVA <abba501@mail.ru>
2026-01-09 11:20:05 +0300

    09.01.2026

---
+++ src/main/java/com/avva_04/InjectProperty.java
@@ -0,0 +1,9 @@
+package com.avva_04;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InjectProperty {
+    String value() default "";
+}
