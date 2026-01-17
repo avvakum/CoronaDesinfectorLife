4e67c883b726af540625e7d7326983fcfd0ca9eb
AVVA <abba501@mail.ru>
2026-01-17 12:25:25 +0300

    17.01.2026

---
+++ src/main/java/com/avva_09/DeprecatedHandlerProxyConfigurator.java
@@ -0,0 +1,22 @@
+package com.avva_09;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class DeprecatedHandlerProxyConfigurator implements ProxyConfigurator {
+    @Override
+    public Object replaceWithProxyIfNeeded(Object t, Class implClass) {
+        if (implClass.isAnnotationPresent(Deprecated.class)) {
+            return Proxy.newProxyInstance(implClass.getClassLoader(), implClass.getInterfaces(), new InvocationHandler() {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    System.out.println("************ что ж ты делаешь урод! ");
+                    return method.invoke(t);
+                }
+            });
+        } else {
+            return t;
+        }
+    }
+}
