0424ac38b0ff4fe1dd6ebf42956b3e1659eca80a
AVVA <abba501@mail.ru>
2026-01-19 11:52:54 +0300

    19.01.2026

---
+++ src/main/java/com/avva_07/InjectByType.java
@@ -0,0 +1,8 @@
+package com.avva_07;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InjectByType {
+}
