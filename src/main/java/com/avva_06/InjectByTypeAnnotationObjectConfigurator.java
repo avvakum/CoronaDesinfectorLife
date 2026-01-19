bd6a4b3dc837ec67963b8550ea25a2623e71cb04
AVVA <abba501@mail.ru>
2026-01-19 11:53:41 +0300

    19.01.2026

---
+++ src/main/java/com/avva_06/InjectByTypeAnnotationObjectConfigurator.java
@@ -0,0 +1,19 @@
+package com.avva_06;
+
+import lombok.SneakyThrows;
+
+import java.lang.reflect.Field;
+
+public class InjectByTypeAnnotationObjectConfigurator implements ObjectConfigurator {
+    @Override
+    @SneakyThrows
+    public void configure(Object t) {
+        for (Field field : t.getClass().getDeclaredFields()) {
+            if (field.isAnnotationPresent(InjectByType.class)) {
+                field.setAccessible(true);
+                Object object = ObjectFactory.getInstance().ceateObject(field.getType());
+                field.set(t, object);
+            }
+        }
+    }
+}
