e1d866b0638133c1372c3b27641db78d1ff19fc0
AVVA <abba501@mail.ru>
2026-01-21 11:36:12 +0300

    21.01.2026

---
+++ src/main/java/com/avva_09/ConsoleAnnouncer.java
@@ -0,0 +1,11 @@
+package com.avva_09;
+
+public class ConsoleAnnouncer implements Announcer {
+    @InjectByType
+    private Recommendator recommendator;
+    @Override
+    public void announce(String message) {
+        System.out.println(message);
+        recommendator.recommend();
+    }
+}
