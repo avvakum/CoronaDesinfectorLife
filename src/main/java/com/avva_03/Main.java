a2d316bfb29e3ac742a38dcfd23ec02ca72f7f69
AVVA <abba501@mail.ru>
2026-01-23 11:36:05 +0300

    23.01.2026

---
+++ src/main/java/com/avva_03/Main.java
@@ -0,0 +1,17 @@
+package com.avva_03;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CoronaDesinfector desinfector = new CoronaDesinfector();
+        desinfector.start(new Room());
+    }
+}
+
+//---------( avva_03 )----------
+//Начинаем дезинфекцию, все вон!!!
+//Всех убью! Вон пошли!
+//Зачитывается молитва: 'корона изыди!' - молитва прочитана, вирус низвергнут в ад
+//Рискните зайти обратно.
+//------------------------------
+
