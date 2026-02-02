eac934cb1ede4b0f94f4b75e08df83f27ce2bd12
AVVA <abba501@mail.ru>
2026-02-02 12:28:02 +0300

    02.02.2026

---
+++ src/main/java/com/avva_10/CoronaDesinfector.java
@@ -0,0 +1,23 @@
+package com.avva_10;
+
+@Deprecated
+public class CoronaDesinfector {
+
+    @InjectByType
+    Announcer announcer;
+    @InjectByType
+    Policeman policeman;
+
+    public void start(Room room ) {
+        System.out.println("---------( avva_10 CGLib )----------");
+        announcer.announce("Начинаем дезинфекцию, все вон!!!");
+        policeman.makePeopleLeaveRoom();
+        desinfect(room);
+        announcer.announce("Рискните зайти обратно.");
+        System.out.println("------------------------------");
+    }
+
+    private void desinfect(Room room) {
+        System.out.println("Зачитывается молитва: 'корона изыди!' - молитва прочитана, вирус низвергнут в ад");
+    }
+}
