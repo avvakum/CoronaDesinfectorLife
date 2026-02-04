7afa5c15de44dff5d4549400502f2bceec2830a6
AVVA <abba501@mail.ru>
2026-02-04 11:56:02 +0300

    04.02.2026

---
+++ src/main/java/com/epam/DeprecatedHandlerProxyConfiguratorImpl.java
@@ -0,0 +1,40 @@
+package com.epam;
+
+
+import org.springframework.cglib.proxy.Enhancer;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class DeprecatedHandlerProxyConfiguratorImpl implements ProxyConfigurator {
+    @Override
+    public Object replaceWithProxyIfNeeded(Object t, Class implClass) {
+        if (implClass.isAnnotationPresent(Deprecated.class)) {
+
+            if (implClass.getInterfaces().length == 0) {
+                return Enhancer.create(implClass, new org.springframework.cglib.proxy.InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        return getInvocationHandlerLogic(method, args, t);
+                    }
+                });
+            }
+
+            return Proxy.newProxyInstance(implClass.getClassLoader(), implClass.getInterfaces(), new InvocationHandler() {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    return getInvocationHandlerLogic(method, args, t);
+                }
+            });
+        } else {
+            return t;
+        }
+    }
+
+    private static Object getInvocationHandlerLogic(Method method, Object[] args, Object t) throws IllegalAccessException, InvocationTargetException {
+        System.out.println("************ что ж ты делаешь epam! ");
+        return method.invoke(t, args);
+    }
+}
