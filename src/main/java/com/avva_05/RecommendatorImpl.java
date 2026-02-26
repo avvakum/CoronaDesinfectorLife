ff06cbdf9bbf89c4ba874c9ad459ae6384b7d9a5
AVVA <abba501@mail.ru>
2026-02-26 12:13:45 +0300

    26.02.2026

---
+++ src/main/java/com/avva_05/RecommendatorImpl.java
@@ -0,0 +1,12 @@
+package com.avva_05;
+
+
+public class RecommendatorImpl implements Recommendator {
+    @InjectProperty("wisky")
+    private String alcohol;
+
+    @Override
+    public void recommend() {
+        System.out.println("to protect from covid-2019 drink " + alcohol);
+    }
+}
