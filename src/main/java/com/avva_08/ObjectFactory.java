dc1fbfec2be23184ebf806092b6e279500266f62
AVVA <abba501@mail.ru>
2026-03-09 11:30:33 +0300

    09.03.2026

---
+++ src/main/java/com/avva_08/ObjectFactory.java
@@ -0,0 +1,54 @@
+package com.avva_08;
+
+
+import lombok.NonNull;
+import lombok.SneakyThrows;
+
+import javax.annotation.PostConstruct;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectFactory {
+    private final ApplicationContext context;
+    private List<ObjectConfigurator> configurators = new ArrayList<>();
+
+    @SneakyThrows
+    public ObjectFactory(ApplicationContext context) {
+        this.context = context;
+
+        for (Class<? extends ObjectConfigurator> aClass : context.getConfig().getScanner().getSubTypesOf(ObjectConfigurator.class)) {
+            configurators.add(aClass.getDeclaredConstructor().newInstance());
+        }
+    }
+
+    @SneakyThrows
+    public <T> T ceateObject(Class<T> implClass) {
+
+        T t = create(implClass);
+
+        configure(t);
+
+        invokeInit(implClass, t);
+
+        return t;
+    }
+
+    private static <T> void invokeInit(Class<T> implClass, T t) throws IllegalAccessException, InvocationTargetException {
+        for (Method method : implClass.getMethods()) {
+            if (method.isAnnotationPresent(PostConstruct.class)) {
+                method.invoke(t);
+            }
+        }
+    }
+
+    private <T> void configure(T t) {
+        configurators.forEach(objectConfigurator -> objectConfigurator.configure(t, context ));
+    }
+
+    private static <T> @NonNull T create(Class<T> implClass) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        T t = implClass.getDeclaredConstructor().newInstance();
+        return t;
+    }
+}
