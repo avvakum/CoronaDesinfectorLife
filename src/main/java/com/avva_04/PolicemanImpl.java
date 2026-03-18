78405cad1a2d49aa45653325cc455939159fb932
AVVA <abba501@mail.ru>
2026-03-18 10:39:38 +0300

    18.03.2026

---
+++ src/main/java/com/avva_04/PolicemanImpl.java
@@ -0,0 +1,8 @@
+package com.avva_04;
+
+public class PolicemanImpl implements Policeman {
+    @Override
+    public void makePeopleLeaveRoom() {
+        System.out.println("Пиф паф, бах бах, кыш кыш!!!");
+    }
+}
