d4e7f68d57940147488b7dd59191e42e1782b5c4
AVVA <abba501@mail.ru>
2026-03-30 11:20:41 +0300

    30.03.2026

---
+++ src/main/java/com/epam/RecommendatorImpl.java
@@ -0,0 +1,16 @@
+package com.epam;
+
+@Singleton
+public class RecommendatorImpl implements Recommendator {
+    @InjectProperty("whisky")
+    private String alcohol;
+
+    public RecommendatorImpl() {
+        System.out.println("Recommendator was created");
+    }
+
+    @Override
+    public void recommend() {
+        System.out.println("to protect from covid-2019,  drink " + alcohol);
+    }
+}
